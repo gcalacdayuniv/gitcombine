3b528a1ffcd07f66906d71783c38d33fe95118af
gcalacdayuniv <gcalacday.univ@gmail.com>
2026-07-27 09:24:10 +0800

    Delete src/utils.js

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-export function isTextFile(path) {
-  const ext = path.split('.').pop().toLowerCase();
-  const textExtensions = [
-    'js', 'ts', 'jsx', 'tsx', 'html', 'css', 'json', 'md', 'txt', 'csv',
-    'py', 'rb', 'go', 'rs', 'php', 'java', 'c', 'cpp', 'h', 'hpp',
-    'sh', 'yml', 'yaml', 'xml', 'toml', 'ini', 'sql', 'gs'
-  ];
-  return textExtensions.includes(ext);
-}
